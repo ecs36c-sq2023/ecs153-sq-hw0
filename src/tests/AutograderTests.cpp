e7c2d188664e3d25c6b0ff115d37311167d24c02
Hongxiang Zhang <hongxiangzhang@campus-043-174.ucdavis.edu>
2024-04-12 13:49:47 -0700

    fix autograder

--- src/tests/AutograderTests.cpp
+++ src/tests/AutograderTests.cpp
@@ -1,26 +1,29 @@
-extern "C" {
+extern "C"
+{
 #include "calculator.h"
 }
 #include <gtest/gtest.h>
 
-char result_str[30];  // Used to store the float as a string
+char result_str[30]; // Used to store the float as a string
 
-TEST(AutograderTests, TestBasic) {
+TEST(AutograderTests, TestBasic)
+{
   Calculator *cal = init_calculator();
   strcpy(cal->expression, "1-3-5-6");
-  float result = parseExpression(*cal);
-  sprintf(result_str, "%.3f", result);
+  float result = parseExpression(cal);
+  snprintf(result_str, 30, "%.3f", result);
   EXPECT_STREQ(result_str, "-13.000");
   free_Calculator(cal);
 }
 
-TEST(AutograderTests, TestBasic2) {
+TEST(AutograderTests, TestBasic2)
+{
   Calculator *cal = init_calculator();
   strcpy(cal->expression, "1+7-8*4");
-  float result = parseExpression(*cal);
-  sprintf(result_str, "%.3f", result);
+  float result = parseExpression(cal);
+  snprintf(result_str, 30, "%.3f", result);
   EXPECT_STREQ(result_str, "-24.000");
   free_Calculator(cal);
-  
+
   // Add more tests here
 }
